0dfdc2b974441c66569f36712f30528754ae4add
Linwish <linwish.me@gmail.com>
2019-06-20 11:43:59 +0800

    modify code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Movie Info
-A simple app for showing movie information. Made with react.
+A web app for showing movie information. Made with react.
+
+## Dependencies
+- React
+
+## Install
+```bash
+npm i
+```
+or
+```bash
+yarn
+```
+
+## Start
+```bash
+npm start
+```
+
+## LICENSE
+MIT
+
--- src/header/header.js
+++ src/header/header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Nav} from './nav';
 import logo from '../logo.svg';
 import './header.css';
 
@@ -9,7 +8,6 @@ export class Header extends React.Component {
     return(
       <header>
         <Link to={'/'}><div className="header"><img src={logo} alt="movie info logo" /> <h1>Movie Info</h1></div></Link>
-        <Nav />
       </header>
     );
   }
